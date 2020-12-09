f7da61c2158cbdefd8adc92176b16934f675ef25
Nicolò <75316126+Nico0402@users.noreply.github.com>
2020-12-09 23:34:37 +0100

    Pubblicazione 23:34 09/12
    Prova pubblicazione progetto

--- Main/master.c
+++ Main/master.c
@@ -8,26 +8,33 @@
 #include <sys/wait.h>
 
 int SO_HOLES;
+int SO_SOURCES;
 
 int main(int argc, char * argv[]){
 	point mappa[SO_HEIGHT][SO_WIDTH];
 	SO_HOLES = atoi(argv[3]);
+	SO_SOURCES = atoi(argv[2]);
 	
 	inizializza_mappa(mappa);
+	genera_holes(mappa);
+	genera_sources(mappa);
 	stampa_mappa(mappa);
 	exit(EXIT_SUCCESS);
 }
 
 void inizializza_mappa(point mappa[SO_HEIGHT][SO_WIDTH]){
-	int i, j, k,riga=0, col=0, cond;
+	int i, j;
 	
 	for(i=0; i<SO_HEIGHT;i++){
 		for(j=0;j<SO_WIDTH;j++){
-			mappa[i][j].val = NOT_HOLES;
+			mappa[i][j].type = NOT_HOLES;
 		}
 	}
-	 
-	k=0;
+}
+
+void genera_holes(point mappa[SO_HEIGHT][SO_WIDTH]){
+	int i, j, k=0,riga=0, col=0, cond;
+	
 	srand(getpid());
 	while(k<SO_HOLES){
 		do{
@@ -35,17 +42,38 @@ void inizializza_mappa(point mappa[SO_HEIGHT][SO_WIDTH]){
 			riga = rand()%SO_HEIGHT;
 			col = rand()%SO_WIDTH;
 			
-			if(mappa[riga][col].val==NOT_HOLES){
+			if(mappa[riga][col].type==NOT_HOLES){
 				for(i=riga-1;i<=riga+1 && !cond;i++){
 					for(j=col-1;j<=col+1 && !cond;j++){
-						if(i>=0 && i<=SO_HEIGHT && j>=0 && j<=SO_WIDTH && mappa[i][j].val==HOLES)
+						if(i>=0 && i<=SO_HEIGHT && j>=0 && j<=SO_WIDTH && mappa[i][j].type==HOLES)
 							cond=1;
 					}
 				}
 				if(!cond)
-					mappa[riga][col].val = HOLES;
+					mappa[riga][col].type = HOLES;
 			}else
 				cond = 1;
+			
+		}while(cond==1);
+		
+		k++;
+	}
+}
+
+void genera_sources(point mappa[SO_HEIGHT][SO_WIDTH]){
+	int i, j, k=0,riga=0, col=0, cond;
+	
+	srand(getpid());
+	while(k<SO_SOURCES){
+		do{
+			cond=0;
+			riga = rand()%SO_HEIGHT;
+			col = rand()%SO_WIDTH;
+			
+			if(mappa[riga][col].type==NOT_HOLES)
+				mappa[riga][col].type = SOURCES;
+			else 
+				cond=1;
 			
 		}while(cond==1);
 		
@@ -66,8 +94,10 @@ void stampa_mappa(point mappa[SO_HEIGHT][SO_WIDTH]){
     	{
     		for(j=0; j<SO_WIDTH; j++)
 		{
-				if(mappa[i][j].val == HOLES)
+				if(mappa[i][j].type == HOLES)
 					printf("| * ");
+				else if(mappa[i][j].type == SOURCES)
+					printf("| S ");
 				else 
 					printf("|   ");
 		}
--- Main/master.h
+++ Main/master.h
@@ -1,7 +1,9 @@
-#define SO_WIDTH 6	/*larghezza*/
-#define SO_HEIGHT 6	/*altezza*/
-#define HOLES 1
+#define SO_WIDTH 15	/*larghezza*/
+#define SO_HEIGHT 15	/*altezza*/
 #define NOT_HOLES 0
+#define HOLES 1
+#define SOURCES 2
+
 #define TEST_ERROR    if (errno) {dprintf(STDERR_FILENO,		\
 					  "%s:%d: PID=%5d: Error %d (%s)\n", \
 					  __FILE__,			\
@@ -11,8 +13,14 @@
 					  strerror(errno));}
 					  
 typedef struct{
-	int val;
+	int type;
+	long time_min;
+	long time_max;
+	int cap_min;
+	int cap_max;
 }point;
 
-void stampa_mappa(point mappa[SO_HEIGHT][SO_WIDTH]);
 void inizializza_mappa(point mappa[SO_HEIGHT][SO_WIDTH]);
+void genera_holes(point mappa[SO_HEIGHT][SO_WIDTH]);
+void genera_sources(point mappa[SO_HEIGHT][SO_WIDTH]);
+void stampa_mappa(point mappa[SO_HEIGHT][SO_WIDTH]);
