b6cf0ad99b4da8bcc7918bcf6e9edffd742dec2e
Nicolò <75316126+Nico0402@users.noreply.github.com>
2020-12-09 23:33:00 +0100

    Revert "Pubblicazione 23:27 09/12"
    This reverts commit 4042728e61a789ca002f14d0596ca68a6a7b9d02.

--- Main/master.c
+++ Main/master.c
@@ -8,33 +8,26 @@
 #include <sys/wait.h>
 
 int SO_HOLES;
-int SO_SOURCES;
 
 int main(int argc, char * argv[]){
 	point mappa[SO_HEIGHT][SO_WIDTH];
 	SO_HOLES = atoi(argv[3]);
-	SO_SOURCES = atoi(argv[2]);
 	
 	inizializza_mappa(mappa);
-	genera_holes(mappa);
-	genera_sources(mappa);
 	stampa_mappa(mappa);
 	exit(EXIT_SUCCESS);
 }
 
 void inizializza_mappa(point mappa[SO_HEIGHT][SO_WIDTH]){
-	int i, j;
+	int i, j, k,riga=0, col=0, cond;
 	
 	for(i=0; i<SO_HEIGHT;i++){
 		for(j=0;j<SO_WIDTH;j++){
-			mappa[i][j].type = NOT_HOLES;
+			mappa[i][j].val = NOT_HOLES;
 		}
 	}
-}
-
-void genera_holes(point mappa[SO_HEIGHT][SO_WIDTH]){
-	int i, j, k=0,riga=0, col=0, cond;
-	
+	 
+	k=0;
 	srand(getpid());
 	while(k<SO_HOLES){
 		do{
@@ -42,38 +35,17 @@ void genera_holes(point mappa[SO_HEIGHT][SO_WIDTH]){
 			riga = rand()%SO_HEIGHT;
 			col = rand()%SO_WIDTH;
 			
-			if(mappa[riga][col].type==NOT_HOLES){
+			if(mappa[riga][col].val==NOT_HOLES){
 				for(i=riga-1;i<=riga+1 && !cond;i++){
 					for(j=col-1;j<=col+1 && !cond;j++){
-						if(i>=0 && i<=SO_HEIGHT && j>=0 && j<=SO_WIDTH && mappa[i][j].type==HOLES)
+						if(i>=0 && i<=SO_HEIGHT && j>=0 && j<=SO_WIDTH && mappa[i][j].val==HOLES)
 							cond=1;
 					}
 				}
 				if(!cond)
-					mappa[riga][col].type = HOLES;
+					mappa[riga][col].val = HOLES;
 			}else
 				cond = 1;
-			
-		}while(cond==1);
-		
-		k++;
-	}
-}
-
-void genera_sources(point mappa[SO_HEIGHT][SO_WIDTH]){
-	int i, j, k=0,riga=0, col=0, cond;
-	
-	srand(getpid());
-	while(k<SO_SOURCES){
-		do{
-			cond=0;
-			riga = rand()%SO_HEIGHT;
-			col = rand()%SO_WIDTH;
-			
-			if(mappa[riga][col].type==NOT_HOLES)
-				mappa[riga][col].type = SOURCES;
-			else 
-				cond=1;
 			
 		}while(cond==1);
 		
@@ -94,10 +66,8 @@ void stampa_mappa(point mappa[SO_HEIGHT][SO_WIDTH]){
     	{
     		for(j=0; j<SO_WIDTH; j++)
 		{
-				if(mappa[i][j].type == HOLES)
+				if(mappa[i][j].val == HOLES)
 					printf("| * ");
-				else if(mappa[i][j].type == SOURCES)
-					printf("| S ");
 				else 
 					printf("|   ");
 		}
